64756c9dc8fc649a156778651f366833c8fcef70
Quentin Barluet de Beauchesne <quentin.debeauchesne@commonecoute.fr>
2012-06-01 17:41:37 +0200

    roster fonctionnel avec le design UN2

--- static/javascript/roster.ui.js
+++ static/javascript/roster.ui.js
@@ -30,33 +30,156 @@ $.uce.Roster.prototype = {
         ucemeeting: null,
         uceclient: null,
         title: "Meeting Roster",
-        mode: 'reduced',
-        owner: new RegExp("producteur"),
-        participant: new RegExp("participant"), 
-        speaker: new RegExp("personnalite"), 
-        default_avatar: null
+        default_avatar: null,
+        speaker_users: $('.users-list:first'),
+        active_users: $('ul.[data-user-list="online"]'),
+        inactive_users: $('ul.[data-user-list="offline"]'),
+        speakers: [],
+        updateInterval: 5000
     },
     // ucengine events
     meetingsEvents: {
         "internal.roster.add"           : "_handleJoin",
-        "internal.roster.delete"        : "_handleLeave"
+        "internal.roster.delete"        : "_handleLeave",
+        "internal.roster.update"        : "_updateRoster"
     },
     _create: function() {
         var that = this;
-        this._state.me = {};
-        this._state.anonCounter = 1;
-        this._updateRoster();
+        this._updateLoop = window.setInterval(function(){
+                that._updateRoster();
+            }, this.options.updateInterval);
+    },
+    /*
+     * Users' state object
+     */
+    _state: {
+        anonCounter: 0,
+        users: {},
+        roster: null,
+        requestRoster: false
+    },
+    getUsersState: function() {
+        return this._state.users;
     },
 
-    clear: function() {},
-    reduce: function() {},
-    expand: function() {},
+    /**
+     * UCE Event handler
+     * "internal.roster.add" Event handler
+    */
+    _handleJoin: function(event) {
+        if (this._state.users[event.from]!==undefined && _.isBoolean(this._state.users[event.from]) === false) {
+            // come back of an old friend
+            this._state.users[event.from].visible = true;
+            //this._updateRoster();
+            return;
+        } 
+        if (_.isBoolean(this._state.users[event.from]) === false) {
+            // first time for this new user
+            // boolean value indicates a request is pending
+            this._state.users[event.from] = true;
+            var that = this;
+            this.options.uceclient.user.get(event.from, function(err, result, xhr) {
+                var visible = that._state.users[event.from];
+                if (err !== null) {
+                    return;
+                }
+                if(result === null) {
+                    return;
+                }
+                that._state.users[event.from] = result.result;
+                that._state.users[event.from].visible = visible;
+                that._state.users[event.from].you = false;
+                that._state.users[event.from].owner = false;
+                that._state.users[event.from].speaker = false;
+
+                if (event.from == that.options.uceclient.uid) {
+                    that._state.users[event.from].you = true;
+                }
+                for (var i=0; i < that.options.speakers.length; i++) {
+                    if (that._state.users[event.from].uid == that.options.speakers[i]){
+                        that._state.users[event.from].speaker = true;
+                    }
+                }
+                event.type = "internal.roster.update";
+                that.options.ucemeeting.trigger(event);
+            });
+            return;
+        }
+    },
 
     /**
-     * Internal functions
+    * "internal.roster.delete" Event handler
+    */
+    _handleLeave: function(event) {
+        if (this._state.users[event.from]!==undefined && _.isBoolean(this._state.users[event.from]) === false) {
+            this._state.users[event.from].visible = false;
+            //this._updateRoster();
+            return;
+        } 
+        if (_.isBoolean(this._state.users[event.from]) === false) {
+            // Lock but indicates absence
+            this._state.users[event.from] = false;
+        }
+    },
+
+    getScreenName: function(uid) {
+        var user = ( this._state.users[uid] !== undefined ) ? this._state.users[uid] : { name: ""};
+        var screenname = user.name;
+        if (user.metadata !== undefined && user.metadata.username !== undefined) {
+            screenname = user.metadata.username;
+        }
+        return screenname;
+    },
+    
+    _sortRoster: function() {
+        this.options.active_users.find('li').sort(function(a, b){
+            return $(a).text() > $(b).text() ? 1 : -1;
+        }).remove().appendTo(this.options.active_users);
+        
+        this.options.inactive_users.find('li').sort(function(a, b){
+            return $(a).text() > $(b).text() ? 1 : -1;
+        }).remove().appendTo(this.options.inactive_users);
+        
+    },
+
+    _updatePosition: function(item) {
+        
+        if ((item.hasClass("user-avatar-personality")) && (item.hasClass("offline-user"))){
+            item.appendTo(this.options.speaker_users);
+        }
+        else if (item.hasClass("user-avatar-personality")){
+            item.prependTo(this.options.speaker_users);
+        }
+        else if (item.hasClass("ui-roster-user-you")){
+           item.prependTo(this.options.active_users);
+        }
+         else if (item.hasClass("connected-user")){
+           item.appendTo(this.options.active_users);
+        }
+        else item.appendTo(this.options.inactive_users);
+    },
+
+    _updateUserState: function(user , useritem){
+        var that = this;
+        for (i=0;i<that._state.roster.length;i++){
+            if (that._state.roster[i].uid==user.uid) {
+                useritem.removeClass("offline-user");
+                useritem.addClass("connected-user");
+                that._updatePosition(useritem);
+                return;
+            }else {
+                useritem.removeClass("connected-user");
+                useritem.addClass("offline-user");
+                that._updatePosition(useritem);
+            }
+        }    
+    },
+    /**
+     * Internal method updating display
      */
-    _updateRoster: function() {
-        this.element.empty();
+    _updateRoster: function(event) {
+        
+        this.element.addClass("ui-roster");
         var meeting = this.options.ucemeeting;
         var users = [];
         $.each(this._state.users, function(uid, user) {
@@ -64,81 +187,72 @@ $.uce.Roster.prototype = {
                 users.push(user);
             }
         });
-        users = users.sort(function(user1, user2) {
-            if (user1.you) {
-                return (-1);
-            }
-            if (user2.you) {
-                return (1);
-            }
-            if (user1.visible) {
-                return (-1);
-            }
-            if (user2.visible) {
-                return (1);
-            }
-            return (0);
-        });
-
+        
         var me = this._state.users[this.options.uceclient.uid];
         var that = this;
+
+        // requête uce
         this.options.ucemeeting.getRoster(function(err, roster){
             that._state.roster=roster;
             $.each(users, function(idx, user) {
+                // Si c'est anonymous on le mets au placard avec Mr Pignon
                 var screenname = that.getScreenName(user.uid);
                 if(screenname === "" || screenname === "anonymous") {
                     return;
                 }
+                /* if user already in the roster block */
                 if($("#"+user.uid).length > 0) {
-                    if(user.visible === true) {
-                        $("#"+user.uid).show();
-                    } else {
-                        $("#"+user.uid).hide();
-                    }
+                    that._updateUserState(user, $("#"+user.uid), $("#"+user.uid).children("a"));
+                    that._sortRoster();
                     return;
                 }
                 // appends new user to roster element
                 var userField = $('<a>');//.attr('href', '/accounts/'+user.name);
-                userField.text(screenname);
+                    userField.text(screenname)
+                    .attr("href","#");
 
                 if (user.metadata && user.metadata.mugshot && user.metadata.mugshot !== "") {
                     userField.prepend($("<figure class='user-avatar'>").append(getMugshot(user.metadata.mugshot)));
                 } else {
-                    userField.prepend($("<figure class='user-avatar'>").append(that.options.default_avatar.clone()));
+                    // FIXME
+                    //userField.prepend($("<figure class='user-avatar'>").append(that.options.default_avatar.clone()));
                 }
                 // role information
-                var roleField = $('<span>');
-
+                var imgField = $('<img>')
+                    .attr("alt", "")
+                    .attr("src", "");
+                var figureField = $('<figure>')
+                    .attr("uid", user.uid)
+                    .addClass('user-avatar')
+                    .append(imgField)
+                    .prependTo(userField);
                 var item = $('<li>')
                     .attr("id", user.uid)
                     .addClass('connected-user')
                     .append(userField)
-                    .append(roleField);
-
-                if (user.uid == that.options.uceclient.uid) {
-                    userField.addClass("ui-roster-user-you");
-                } else if (user.metadata.groups && user.metadata.groups.search(that.options.speaker) != -1) {
-                    roleField.text(that.options.speaker.source);
-                    userField.addClass("ui-roster-user-speaker");
-                } else if (user.metadata.groups && user.metadata.groups.search(that.options.owner) != -1) {
-                    roleField.text(that.options.owner.source);
-                    userField.addClass("ui-roster-user-owner");
-                } else {
-                    userField.addClass("ui-roster-user-active");
+                    .click(function(evt) {
+                        evt.preventDefault();
+                    });
+                
+                for (var i=0; i < that.options.speakers.length; i++) {
+                    if (user.uid == that.options.speakers[i]){
+                        user.speaker = true;
+                    }
                 }
-                item.hide();
-                item.appendTo(that.element);
-            });
-            roster.forEach(function(activeuser){
-                if($("#"+activeuser.uid).length > 0) {
-                    $("#"+activeuser.uid).show();
+                if (user.speaker == true) {
+                    item.addClass("user-avatar-personality");
                 }
+                else if (user.uid == that.options.uceclient.uid) {
+                    item.addClass("ui-roster-user-you");
+                }
+                that._updateUserState(user, item);
+                that._sortRoster();
             });
         });
     },
-
     destroy: function() {
-        this.element.find('*').remove();
+        this.element.removeClass("ui-roster");
+        this.element.find('li').remove();
         $.Widget.prototype.destroy.apply(this, arguments); // default destroy
     }
 };
