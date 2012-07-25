77c13972758fd4be28a245cdbfdfb4286d084e84
Quentin Barluet de Beauchesne <quentin.debeauchesne@commonecoute.fr>
2012-07-25 16:36:47 +0200

    cleaning

--- static/javascript/test_uce.roster.js
+++ static/javascript/test_uce.roster.js
@@ -31,6 +31,9 @@ var MockSpeaker = {
     visible: true
 };
 
+// we go on the roster tab
+$('#player-aside-nav [data-nav="videoticker-users"]').click();
+
 var MockUser = {
     auth: "password",
     domain: "localhost",
@@ -78,77 +81,81 @@ Factories.updateRosterEvent = function(from) {
     };
 }
 
-test("user is complete", function() {
+test("User is complete", function() {
     expect(5);
-    // nettoyage
-    $("#"+MockUser.uid).remove();
-    $('#roster').data("roster")._state.users[MockUser.uid]=MockUser;
-    // test
+    // Initialize
+    $('#roster').data("roster")._state.users[MockUser.uid] = MockUser;
     $('#roster').data("roster")._updateUser(MockUser);
+    // Testing
     notEqual($("#"+MockUser.uid).length, 0, "User exist");
     equal($("#"+MockSpeaker.uid).hasClass("user-avatar-personality"), false, "User is not in speaker section");
-    /// tester la présence d'un nom 
     notEqual($("#"+MockUser.uid).find('a').text().length, 0, "User has a name");
     equal($("#"+MockUser.uid).find('a').text()===MockUser.name, true, "User has the good name");
-    /// tester la présence d'un avatar 
     notEqual($("#"+MockUser.uid).find('img').attr('src').length, 0, "User has an avatar");
+    // Cleaning
+    $("#"+MockUser.uid).remove();
 });
 
-test("speaker is in the right place", function() {
+test("Speaker is in the right place", function() {
     expect(3);
-    // nettoyage
-    $("#"+MockSpeaker.uid).remove();
-    $('#roster').data("roster")._state.users[MockSpeaker.uid]=MockSpeaker;
-    // test
+    // Initialize
+    $('#roster').data("roster")._state.users[MockSpeaker.uid] = MockSpeaker;
     $('#roster').data("roster").options.speakers.push(MockSpeaker.uid);
     $('#roster').data("roster")._updateUser(MockSpeaker);
+    // Testing
     equal($("#"+MockSpeaker.uid).length, 1, "Speaker exist");
     equal($("#"+MockSpeaker.uid).hasClass("user-avatar-personality"), true, "Speaker has class personality");
     equal($("#"+MockSpeaker.uid).find('img').css('box-shadow')==="none", false, "Speaker has blue shadow");
+    // Cleaning
+    $("#"+MockSpeaker.uid).remove();
 });
 
-test("user connection", function() {
+test("User connection", function() {
     expect(6);
-    // nettoyage
-    $("#"+MockUser.uid).remove();
-    $('#roster').data("roster")._state.users[MockUser.uid]=MockUser;
-    // test
-	if ($('#roster').data("roster")._state.roster === null){
+    // Initialize
+    $('#roster').data("roster")._state.users[MockUser.uid] = MockUser;
+    if ($('#roster').data("roster")._state.roster === null){
         $('#roster').data("roster")._state.roster = [];
     }
-	//connecté
+    // Online
     $('#roster').data("roster")._state.roster.push(MockUser);
     $('#roster').data("roster")._state.rosterUidList = $.map($('#roster').data("roster")._state.roster, function(connecteduser){ return connecteduser.uid });
     $('#roster').data("roster")._updateUser(MockUser);
     equal($("#"+MockUser.uid).length, 1, "User is visible (online test)");
     equal($("#"+MockUser.uid).hasClass("connected-user"), true, "User is online (has class online)");
     equal($("#"+MockUser.uid).hasClass("offline-user"), false, "User is online (has not class offline)");
-	// déconnecté
-	$('#roster').data("roster")._state.roster = jQuery.grep($('#roster').data("roster")._state.roster, function(value) {
+    // Offline
+    $('#roster').data("roster")._state.roster = jQuery.grep($('#roster').data("roster")._state.roster, function(value) {
           return value != MockUser;
         });
-	$('#roster').data("roster")._state.rosterUidList = $.map($('#roster').data("roster")._state.roster, function(connecteduser){ return connecteduser.uid });
-	$('#roster').data("roster")._updateUser(MockUser);
-	equal($("#"+MockUser.uid).length, 1, "User is visible (offline test)");
+    $('#roster').data("roster")._state.rosterUidList = $.map($('#roster').data("roster")._state.roster, function(connecteduser){ return connecteduser.uid });
+    $('#roster').data("roster")._updateUser(MockUser);
+    equal($("#"+MockUser.uid).length, 1, "User is visible (offline test)");
     equal($("#"+MockUser.uid).hasClass("connected-user"), false, "User is offline (has not class online)");
     equal($("#"+MockUser.uid).hasClass("offline-user"), true, "User is offline (has class offline)");
+    // Cleaning
+    $("#"+MockUser.uid).remove();
 });
 
-test("user click test", function() {
+test("User click test", function() {
     expect(8);
-    // nettoyage
-	equal($("#"+MockUser.uid).length, 1, "User is visible");
-	//filtrage
-	$("#"+MockUser.uid).trigger('click');
-	equal($('#player-aside-nav [data-nav="videoticker-comments"]').hasClass("active"), true, "Tab switched (comments tab active)");
-	equal($('#player-aside-nav [data-nav="videoticker-users"]').hasClass("active"), false, "Tab switched (roster tab not active)");
-	equal($(".clone[id='"+MockUser.uid+"']").length, 1, "Clone of selected user exist");
+    // Initialize
+    $('#roster').data("roster")._state.users[MockUser.uid] = MockUser;
+    $('#roster').data("roster")._updateUser(MockUser);
+    equal($("#"+MockUser.uid).length, 1, "User is visible");
+    // Filtering
+    $("#"+MockUser.uid).trigger('click');
+    equal($('#player-aside-nav [data-nav="videoticker-comments"]').hasClass("active"), true, "Tab switched (comments tab active)");
+    equal($('#player-aside-nav [data-nav="videoticker-users"]').hasClass("active"), false, "Tab switched (roster tab not active)");
+    equal($(".clone[id='"+MockUser.uid+"']").length, 1, "Clone of selected user exist");
     notEqual($(".clone[id='"+MockUser.uid+"']").find('a').text().length, 0, "Clone has a name");
     equal($(".clone[id='"+MockUser.uid+"']").find('a').text()===MockUser.name, true, "Clone has the good name");
     notEqual($(".clone[id='"+MockUser.uid+"']").find('img').attr('src').length, 0, "Clone has an avatar");
-	// "dé"-filtrage
-	$(".clone[id='"+MockUser.uid+"']").trigger('click');	
-	notEqual($(".clone[id='"+MockUser.uid+"']").length, 1, "Clone of selected user is well suppressed after click");
+    // Unfiltering
+    $(".clone[id='"+MockUser.uid+"']").trigger('click');    
+    notEqual($(".clone[id='"+MockUser.uid+"']").length, 1, "Clone of selected user is well suppressed after click");
+    // Cleaning
+    $("#"+MockUser.uid).remove();
 });
 
 
